24b755e5da7f94940e917a8802f95f19f1a6494d
yoonsik(for private project) <yoonsik@sin-yunsig-ui-MacBookPro.local>
2023-07-25 21:37:58 +0900

    jpa를 활용한 member insert코드작성

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,19 +14,27 @@ public class Main {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
+        try{
+            insertMember(tx, em);
+
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            // entityManager가 데이터베이스 커넥션을 가지고 동작하므로 사용후에 닫아주어야 한다.
+            em.close();
+        }
+        emf.close();
+
+        System.out.println("Hello world!");
+    }
+
+    private static void insertMember(EntityTransaction tx, EntityManager em){
         Member member = new Member();
-        member.setId(1L);
-        member.setName("yoonsik");
+        member.setId(3L);
+        member.setName("leo");
 
         em.persist(member);
 
         tx.commit();
-
-        em.close();
-        emf.close();
-
-        System.out.println("Hello world!");
-
-
     }
 }
