b0bdb9eb084d38e012b84d6687f6c170aeb79fda
shinyoonsik <yoonsik141@gmail.com>
2023-08-29 01:05:54 +0900

    임베디드 타입은 레퍼런스 타입이므로 call by ref에 의한 side-effect발생 가능함

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,12 +19,24 @@ public class Main {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setName("yoonsik");
-            member.setAddress(new Address("city", "street", "10000"));
-            member.setWorkPeriod(new Period());
+            Address address = new Address("city", "street", "10000");
+            Member member1 = new Member();
+            member1.setName("member1");
+            member1.setAddress(address);
+            em.persist(member1);
 
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setName("member2");
+
+            // 임베디드타입을 공유해서 쓰면 레퍼런스를 참조하므로 의도치않게 member2의 address도 변경된다 => call by ref
+            // 따라서, 의도하지 않았면 값을 복사해서 사용하자
+            member1.getAddress().setCity("hello!");
+
+            // sol, member2는 복사한 copyAddress를 사용하자
+            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+            member2.setAddress(copyAddress);
+
+            em.persist(member2);
 
             tx.commit();
         } catch (Exception e) {
